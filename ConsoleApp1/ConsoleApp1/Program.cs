ffbb50c4101ec8800a1a0981860930f4aaf29902
s33864 <7ckaminski@gmail.com>
2026-03-11 10:47:34 +0100

    Zmiana 1 i 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World1!");
+
+Console.WriteLine("Hello, World2!");
